8998bea6b21d9212ffd02c882a268c4f8ba6a2d0
lijiantao <825067852@qq.com>
2018-02-06 15:59:59 +0800

    edit

--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ButtonPage} from '../button/button';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  buttonPage;
 
   constructor(public navCtrl: NavController) {
+    this.buttonPage=ButtonPage;
 
   }
 
