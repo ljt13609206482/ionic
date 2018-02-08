98acdb9e34cfd0091c0cf04f70e23adc75ffdc76
lijiantao <825067852@qq.com>
2018-02-08 15:38:17 +0800

    edit

---
+++ serve-demo/app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by web-01 on 2018/2/8.
+ */
+const express=require('express');
+let app=new express();
+app.get('/',(req,res)=>{
+   //req:request
+   // res:response
+    res.end("it works...")
+})
+
+//设置服务器端口号
+app.listen(3000);
