dee42a4866205c6189001d19c8bc65f90ca3f512
lijiantao <825067852@qq.com>
2018-02-08 17:54:40 +0800

    edit

--- serve-demo/app.js
+++ serve-demo/app.js
@@ -1,16 +1,27 @@
 /**
  * Created by web-01 on 2018/2/8.
  */
+//引入依赖模块
 const express=require('express');
+const bodyParser=require('body-parser');
+
+//实例化一各对象
 let app=new express();
+//配置中间件
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get('/',(req,res)=>{
    //req:request
    // res:response
     res.end("it works...")
 });
-app.get('/signUp',(req,res)=>{
+app.post('/signUp',(req,res)=>{
     //...
-    let email=req.query.email;
+    //get:req.query.*
+    let email=req.body.email;
+    let username=req.body.username;
+    let password=req.body.password;
+
     res.end(`email:${email}`)
 })
 
