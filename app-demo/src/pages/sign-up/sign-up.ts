1908c2bd93dd31876beca2bcdd1f04f4535b26d5
lijiantao <825067852@qq.com>
2018-02-08 19:01:11 +0800

    edit

--- app-demo/src/pages/sign-up/sign-up.ts
+++ app-demo/src/pages/sign-up/sign-up.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {HttpClient} from '@angular/common/http';
 
 /**
  * Generated class for the SignUpPage page.
@@ -11,7 +12,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @IonicPage()
 @Component({
   selector: 'page-sign-up',
-  templateUrl: 'sign-up.html',
+  templateUrl: 'sign-up.html'
 })
 export class SignUpPage {
   //定义对象保存用户基本信息
@@ -24,7 +25,7 @@ export class SignUpPage {
     city:'BeiJing'
   };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public httpClient: HttpClient) {
   }
 
   ionViewDidLoad() {
@@ -33,6 +34,14 @@ export class SignUpPage {
   signUp():void{
     console.error(this.user);
     //发送HTTP请求
+    let url='http://127.0.0.1:3000/signUp';
+    this.httpClient.post(url,{email:this.user.email,password:this.user.password})
+      .subscribe((res)=>{
+        console.errer(res)
+      }),
+      (err)=>{
+        console.error(err)
+      }
   }
 
 }
