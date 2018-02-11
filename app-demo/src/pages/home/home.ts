94b018b03a282ca5d54349b2ebed85202bd2e5f2
lijiantao <825067852@qq.com>
2018-02-11 15:30:20 +0800

    edit

--- app-demo/src/pages/home/home.ts
+++ app-demo/src/pages/home/home.ts
@@ -7,11 +7,19 @@ import {IonicPage, NavController} from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  //定义user对象，保存用户输入的邮箱以及密码
+  user={
+    email:'',
+    password:''
+  };
   constructor(public navCtrl: NavController) {
   }
 
   signUpPage():void {
     this.navCtrl.push('SignUpPage');
+  }
+  signIn():void{
+    //发送http请求
+    console.error(this.user)
   }
 }
